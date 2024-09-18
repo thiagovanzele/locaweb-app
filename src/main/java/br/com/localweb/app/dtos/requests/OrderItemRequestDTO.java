db9353d6d5863c2140658a372c7fd45293dfb44b
Thiago Vanzele <thiagovanzele@gmail.com>
2024-09-17 21:29:15 -0300

    feat: New validations add in Order Request DTO

--- src/main/java/br/com/localweb/app/dtos/requests/OrderItemRequestDTO.java
+++ src/main/java/br/com/localweb/app/dtos/requests/OrderItemRequestDTO.java
@@ -1,6 +1,7 @@
 package br.com.localweb.app.dtos.requests;
 import br.com.localweb.app.domain.enums.ProductType;
+import jakarta.validation.constraints.NotBlank;
 
-public record OrderItemRequestDTO(Integer quantity, ProductType productType) {
+public record OrderItemRequestDTO(@NotBlank(message = "Quantity cannot be zero.") Integer quantity, @NotBlank(message = "Product cannot be null.") ProductType productType) {
 
 }
--- src/main/java/br/com/localweb/app/dtos/requests/OrderRequestDTO.java
+++ src/main/java/br/com/localweb/app/dtos/requests/OrderRequestDTO.java
@@ -1,8 +1,11 @@
 package br.com.localweb.app.dtos.requests;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.List;
 import java.util.UUID;
 
-public record OrderRequestDTO(UUID clientID, List<OrderItemRequestDTO> orderItems) {
+public record OrderRequestDTO(@NotNull(message = "A client is required to place the order.") UUID clientID, @NotEmpty(message = "Order must have at least one item.") List<OrderItemRequestDTO> orderItems) {
 
 }
