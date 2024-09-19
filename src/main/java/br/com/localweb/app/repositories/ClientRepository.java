df1119deeb4efc9b5648bea3ecd6b3178159274a
Thiago Vanzele <thiagovanzele@gmail.com>
2024-09-19 20:50:56 -0300

    feat: Query to verify if client is active

--- src/main/java/br/com/localweb/app/repositories/ClientRepository.java
+++ src/main/java/br/com/localweb/app/repositories/ClientRepository.java
@@ -1,9 +1,19 @@
 package br.com.localweb.app.repositories;
 
 import br.com.localweb.app.domain.client.Client;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface ClientRepository extends JpaRepository <Client, UUID> {
+
+    @Query("""
+            select c.active
+            from Client c
+            where
+            c.id = :id
+            """)
+    public Boolean findActiveById(@NotNull UUID id);
 }
