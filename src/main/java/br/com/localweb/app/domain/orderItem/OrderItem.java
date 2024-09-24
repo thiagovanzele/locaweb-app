45b6280519d0df8e7917cb596b03828f4926151f
Thiago Vanzele <thiagovanzele@gmail.com>
2024-09-24 19:01:14 -0300

    refactor: OrderItemPK add in OrderItem

--- src/main/java/br/com/localweb/app/domain/orderItem/OrderItem.java
+++ src/main/java/br/com/localweb/app/domain/orderItem/OrderItem.java
@@ -1,14 +1,12 @@
 package br.com.localweb.app.domain.orderItem;
 
 import br.com.localweb.app.domain.order.Order;
+import br.com.localweb.app.domain.pk.orderItemPK.OrderItemPK;
 import br.com.localweb.app.domain.product.Product;
 import br.com.localweb.app.dtos.requests.OrderItemRequestDTO;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.UUID;
 
@@ -18,28 +16,43 @@ import java.util.UUID;
 @AllArgsConstructor
 @Setter
 @Getter
+@EqualsAndHashCode(of = "id")
 public class OrderItem {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID id;
-
-    @ManyToOne
-    private Product product;
-
-    @JsonIgnore
-    @ManyToOne
-    private Order order;
+    @EmbeddedId
+    private OrderItemPK id = new OrderItemPK();
 
     private Integer quantity;
 
     private Double subTotal;
 
-    public OrderItem(Product product, OrderItemRequestDTO data) {
-        this.product = product;
-        this.order = new Order();
-        this.quantity = data.quantity();
-        this.subTotal = data.productType().getPrice() * data.quantity();
+    public OrderItem(Order order, Product product, Integer quantity) {
+        this.id.setOrder(order);
+        this.id.setProduct(product);
+        this.quantity = quantity;
+        this.subTotal = product.getValue() * quantity;
+    }
+
+    @JsonIgnore
+    public Order getOrder() {
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
+
+    @JsonIgnore
+    public OrderItemPK getId() {
+        return id;
+    }
+
+    public Product getProduct() {
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product) {
+        id.setProduct(product);
     }
 
 
